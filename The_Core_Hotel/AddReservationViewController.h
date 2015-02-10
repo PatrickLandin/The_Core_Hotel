bb566681c2ffe73825aaecd1df48bf6d18132483
Patrick Landin <patricklandin@gmail.com>
2015-02-10 13:14:26 -0800

    Created VCs for reservations.

---
+++ The_Core_Hotel/AddReservationViewController.h
@@ -0,0 +1,13 @@
+//
+//  AddReservationViewController.h
+//  The_Core_Hotel
+//
+//  Created by Patrick Landin on 2/10/15.
+//  Copyright (c) 2015 pLandin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AddReservationViewController : UIViewController
+
+@end
---
+++ The_Core_Hotel/AvailabilityViewController.h
@@ -0,0 +1,13 @@
+//
+//  AvailabilityViewController.h
+//  The_Core_Hotel
+//
+//  Created by Patrick Landin on 2/10/15.
+//  Copyright (c) 2015 pLandin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AvailabilityViewController : UIViewController
+
+@end
