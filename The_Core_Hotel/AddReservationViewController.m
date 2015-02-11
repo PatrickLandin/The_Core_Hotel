b8ed9f33978654a67b0252a64dd5974512dde665
Patrick Landin <patricklandin@gmail.com>
2015-02-11 14:01:37 -0800

    Set up testing for adding reservations.

--- The_Core_Hotel/AddReservationViewController.m
+++ The_Core_Hotel/AddReservationViewController.m
@@ -9,6 +9,7 @@
 #import "AddReservationViewController.h"
 #import "Reservation.h"
 #import "Guest.h"
+#import "HotelService.h"
 
 @interface AddReservationViewController ()
 @property (weak, nonatomic) IBOutlet UIDatePicker *startDate;
@@ -28,24 +29,37 @@
     // Dispose of any resources that can be recreated.
 }
 - (IBAction)makeResPressed:(id)sender {
-  Reservation *reservation = [NSEntityDescription insertNewObjectForEntityForName:@"Reservation" inManagedObjectContext:self.selectedRoom.managedObjectContext];
   
-  reservation.startDate = self.startDate.date;
-  reservation.endDate = self.endDate.date;
-  reservation.room = self.selectedRoom;
-  Guest *guest = [NSEntityDescription insertNewObjectForEntityForName:@"Guest" inManagedObjectContext:self.selectedRoom.managedObjectContext];
+  Guest *guest = [NSEntityDescription insertNewObjectForEntityForName:@"Guest" inManagedObjectContext:[[HotelService sharedService] coreDataStack].managedObjectContext];
   guest.firstName = @"Patrick";
   guest.lastName = @"Landin";
-  reservation.guest = guest;
   
-  NSLog(@"%lu",(unsigned long)self.selectedRoom.reservations.count);
-  
-  NSError *saveError;
-  [self.selectedRoom.managedObjectContext save:&saveError];
-  
-  if (saveError) {
-    NSLog(@" %@",saveError.localizedDescription);
-  }
+  [[HotelService sharedService] bookReservationForGuest:guest forRoom:self.selectedRoom startDate:self.startDate.date endDate:self.endDate.date];
+  [self dismissViewControllerAnimated:true completion:nil];
 }
+
+
+  // Code for reserving a stay before singleton:
+  
+  
+  
+//  Reservation *reservation = [NSEntityDescription insertNewObjectForEntityForName:@"Reservation" inManagedObjectContext:self.selectedRoom.managedObjectContext];
+//  
+//  reservation.startDate = self.startDate.date;
+//  reservation.endDate = self.endDate.date;
+//  reservation.room = self.selectedRoom;
+//  Guest *guest = [NSEntityDescription insertNewObjectForEntityForName:@"Guest" inManagedObjectContext:self.selectedRoom.managedObjectContext];
+//  guest.firstName = @"Patrick";
+//  guest.lastName = @"Landin";
+//  reservation.guest = guest;
+//  
+//  NSLog(@"%lu",(unsigned long)self.selectedRoom.reservations.count);
+//  
+//  NSError *saveError;
+//  [self.selectedRoom.managedObjectContext save:&saveError];
+//  
+//  if (saveError) {
+//    NSLog(@" %@",saveError.localizedDescription);
+//  }
 
 @end
--- The_Core_Hotel/HotelService.m
+++ The_Core_Hotel/HotelService.m
@@ -36,6 +36,20 @@
   return self;
 }
 
--(Reservation)
+-(Reservation *)bookReservationForGuest:(Guest *)guest ForRoom:(Room *)room startDate:(NSDate*)startDate endDate:(NSDate *)endDate {
+  Reservation *reservation = [NSEntityDescription insertNewObjectForEntityForName:@"Reservation" inManagedObjectContext:self.coreDataStack.managedObjectContext];
+  reservation.startDate = startDate;
+  reservation.endDate = endDate;
+  reservation.room = room;
+  reservation.guest = guest;
+  
+  NSError *saveError;
+  [self.coreDataStack.managedObjectContext save:&saveError];
+  if (!saveError) {
+    return reservation;
+  } else {
+    return nil;
+  }
+}
 
 @end
